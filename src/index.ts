3b1836f0e951bede6fea062a0771ce88ca1dc86e
PK <peter.kieltyka@gmail.com>
2026-03-12 12:58:49 -0400

    v0.1.2

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { getDefaultProvider, getDefaultModel, showConfig, setDefaultModel, setDe
 // Version
 // ---------------------------------------------------------------------------
 
-const VERSION = "0.1.1";
+const VERSION = "0.1.2";
 
 // ---------------------------------------------------------------------------
 // Help text
